e016c0029587bb8a00b43c1445b42b77c040259f
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2025-11-07 18:33:00 -0800

    sanitize

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
 import { handleGet } from './handlers/routeHandlers.js'
+import { handlePost } from './handlers/routeHandlers.js'
 
 
 const PORT  = 8000
@@ -12,6 +13,9 @@ const server = http.createServer(async (req, res) => {
     if (req.url === '/api') {
         if (req.method === 'GET') {
             return await handleGet(res)
+        }
+        else if (req.method === 'POST') {
+            handlePost(req, res)
         }
     }
     else if (!req.url.startsWith('/api')) {
